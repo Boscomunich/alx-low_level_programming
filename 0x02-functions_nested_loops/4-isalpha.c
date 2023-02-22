b8b809522e2bd80bd2ca362b3d56048f84ffd44c
obuegbe solomon <obuegbechidera@gmail.com>
2023-02-22 02:17:31 -0800

    task 1 done

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,24 +1,14 @@
-#include "main.h"
+include "main.h"
 
 /**
-
- * _isalpha-check if character is lowercase or uppercase
-
- * @c:character to check if it islowercase.
-
- * Return:1-if lowercase/uppercase,0-if not.
-
+ * _isalpha - checks for alpahabet
+ * @c: character to be checked
+ * Return: 1 if character is a letter,0 otherwise
  */
-
 int _isalpha(int c)
-
 {
-	  if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	  {
-		  return (1);
-	  }
-	  else
-	  {
-		  return (0)
-	  }
+	if ((c >= 'a', &&c <= 'z') || (c >= 'A', &&c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
