7a48c517f65aab2aa2a6970f4bc71e9b2eabaf48
obuegbe solomon <obuegbechidera@gmail.com>
2023-02-22 11:45:30 -0800

    task 11 done

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,12 @@
 #include "main"
+
+/**
+ * add - ads two numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: the addition of both
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
